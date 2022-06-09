9ed3ff2319efc81553961906325d82a315323914
AndrewSigorskih <crescent8547@yandex.ru>
2022-06-10 01:14:40 +0300

    Update and rename revc.py to 03_revc.py

--- Bioinformatics Stronghold/03_revc.py
+++ Bioinformatics Stronghold/03_revc.py
@@ -1,4 +1,4 @@
 from Bio.Seq import Seq
-with open ('rosalind_revc.txt') as infile:
+with open ('rosalind_revc.txt', 'r') as infile:
     sequence = Seq(infile.readline().strip('\n'))
 print(str(sequence.reverse_complement()))
