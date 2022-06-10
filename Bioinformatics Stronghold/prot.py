9fb15e4d3efd1c6319ac8d551be8c366593acc58
AndrewSigorskih <crescent8547@yandex.ru>
2022-06-10 12:17:37 +0300

    Delete prot.py

--- Bioinformatics Stronghold/prot.py
+++
@@ -1,6 +0,0 @@
-from Bio.Seq import Seq
-with open('rosalind_prot.txt') as infile:
-    matrix = Seq(infile.readline().strip('\n'))
-with open('out.txt', 'w') as outfile:
-    outfile.write(str(matrix.translate()).strip('*'))
-
