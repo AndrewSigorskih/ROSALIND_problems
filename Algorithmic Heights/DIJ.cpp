4488205ce02e03cf284f42fc1d54712b27c73cf7
AndrewSigorskih <crescent8547@yandex.ru>
2022-08-07 19:35:22 +0300

    Dijkstra's Algorithm: redundant std::pair removed

--- Algorithmic Heights/DIJ.cpp
+++ Algorithmic Heights/DIJ.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <vector>
 #include <list>
-#include <utility> //pair
 #include <set> // imitating proirity queue
 #include <climits> // UINT_MAX
 
@@ -17,11 +16,18 @@ struct adjNode
 };
 using adjList = std::vector<std::list<adjNode>>;
 
+bool operator<(const adjNode& a, const adjNode& b)
+{ // custom comparator for usage in set
+    if (a.weight != b.weight)
+        return (a.weight < b.weight);
+    else
+        return (a.name < b.name);
+}
+
 class Graph
 {
     unsigned edgesNum = 0;
     adjList nodes;
-    std::vector<bool> visited;
     std::vector<unsigned> path;
 
 public:
@@ -31,7 +37,6 @@ public:
     Graph(unsigned verticesNum) 
     {
         this->nodes = adjList(verticesNum);
-        this->visited = std::vector<bool>(verticesNum, false);
         this->path = std::vector<unsigned>(verticesNum, UINT_MAX);
     }
 
@@ -55,14 +60,14 @@ void Graph::Dijkstra(unsigned s)
 {
     //init
     this->path[s] = 0;
-    std::set< std::pair<unsigned, unsigned> > queue;
-    queue.insert(std::make_pair(0, s));
+    std::set<adjNode> queue;
+    queue.insert(adjNode{s, 0});
 
     while (!queue.empty())
     {
-        std::pair<unsigned, unsigned> tmp = *(queue.begin());
+        adjNode tmp = *(queue.begin());
         queue.erase(queue.begin());
-        unsigned u = tmp.second;
+        unsigned u = tmp.name;
         //iterate all neighbours
         for (auto it = this->nodes[u].begin(); it != this->nodes[u].end(); ++it)
         {
@@ -73,9 +78,9 @@ void Graph::Dijkstra(unsigned s)
             if (this->path[v] > this->path[u] + weight)
             {
                 if (this->path[v] != UINT_MAX)
-                    queue.erase(queue.find(std::make_pair(this->path[v], v)));
+                    queue.erase(queue.find(adjNode{v, this->path[v]}));
                 this->path[v] = this->path[u] + weight;
-                queue.insert(std::make_pair(this->path[v], v));
+                queue.insert(adjNode{v, this->path[v]});
             }
         }
     }
@@ -92,7 +97,7 @@ void Graph::printPath(std::ofstream& ofs)
         if (i < (this->vNum()-1))
             ofs << " ";
     }
-    //ofs << '\n';
+    ofs << '\n';
 }
 
 int main()
@@ -125,6 +130,6 @@ int main()
     }
 
     graph.printPath(ost);
-    
+
     return 0;
 }
